9e9ec0c7dc5107a1a4a4223fc4fc81e942e2ed3a
fightingggg <121657134+fightingggg@users.noreply.github.com>
2023-01-01 13:35:42 +0900

    Add files via upload

---
+++ gugu.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class gugu {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("몇단인지 입력하시오");
+		
+		int a = s.nextInt();
+		int b = 1;
+		
+		System.out.println(a+"x"+b+"="+a*b);
+		b++;
+		System.out.println(a+"x"+b+"="+a*b);
+		b++;
+		System.out.println(a+"x"+b+"="+a*b);
+		b++;
+		System.out.println(a+"x"+b+"="+a*b);
+		b++;
+		System.out.println(a+"x"+b+"="+a*b);
+		b++;
+		System.out.println(a+"x"+b+"="+a*b);
+		b++;
+		System.out.println(a+"x"+b+"="+a*b);
+		b++;
+		System.out.println(a+"x"+b+"="+a*b);
+		b++;
+		System.out.println(a+"x"+b+"="+a*b);
+		b++;
+		
+
+	}
+
+
+}
