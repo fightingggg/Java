57a1195433e6e53607b9f86f88078ec6424ea7f9
fightingggg <121657134+fightingggg@users.noreply.github.com>
2023-01-03 10:49:04 +0900

    Add files via upload

---
+++ primenumber.java
@@ -0,0 +1,26 @@
+
+public class primenumber {
+	
+   public static void main(String[] args) {
+	   
+	   int a=0;
+
+       for(int i=2; i<=100; i++) // 1은 소수가 아니므로 2부터 시작
+       {
+           for(int j=2; j<=i; j++)
+           {
+                if(i%j ==0) 
+                {
+                     a ++;
+                }    
+           }
+           
+           // 소수는 1과 자기자신으로만 나눠지므로 자기자신으로 한번만 나눠질때 소수
+           if(a==1)
+           {
+                System.out.print(i+" ");
+           }
+           a=0;
+       }
+   }
+}
