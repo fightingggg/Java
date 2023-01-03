3a98b3496a77f6176a0645d46e44c2d580bc3fc4
fightingggg <121657134+fightingggg@users.noreply.github.com>
2023-01-03 11:18:26 +0900

    Update study.java

--- study.java
+++ study.java
@@ -8,6 +8,8 @@ public class study {
 	   String str7 ="John,Jane,Jame,Jason,Jacob,Johanson,Jefferson";
 	   String[] token = str7.split(",");
 	   
+	   System.out.println(Arrays.toString(token));
+	   
 	   
 	   for(int i=0; i<token.length; i++) {
 		   System.out.println(token[i]);
