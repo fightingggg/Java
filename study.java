a3d6b38edef88c494c782ba36bf826b81a7518cd
fightingggg <121657134+fightingggg@users.noreply.github.com>
2023-01-03 12:30:19 +0900

    Add files via upload

--- study.java
+++ study.java
@@ -1,18 +1,38 @@
-import java.util.Arrays;
+
 
 public class study {
+	public static void main(String[] args) {
 
-   public static void main(String[] args) {
-  
+	String str = "Good Morning Vietnam";
+	String sum ="";
+    
+   
+    for(int i = 19, j =20; j>0; i--,j--) {
+//       System.out.print(str.substring(i,j));
+       sum+=str.substring(i,j);
+    }
+    System.out.println(sum);
+    
 
-	   String str7 ="John,Jane,Jame,Jason,Jacob,Johanson,Jefferson";
-	   String[] token = str7.split(",");
+
+
+
+
+
+
+
 	   
-	   System.out.println(Arrays.toString(token));
 	   
 	   
-	   for(int i=0; i<token.length; i++) {
-		   System.out.println(token[i]);
-	   }
+	   
+	   
+	   
+//	   String str7 ="John,Jane,Jame,Jason,Jacob,Johanson,Jefferson";
+//	   String[] token = str7.split(",");
+//	   
+//	   
+//	   for(int i=0; i<token.length; i++) {
+//		   System.out.println(token[i]);
+	   
    }
 }
